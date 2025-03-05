3b20b8687543ffcfb004e6a8b7044e1ea9a54390
victoriaBuenx <victoriabuenomijares@gmail.com>
2025-03-04 23:31:49 -0600

    revert

--- src/clinicadental/Principal.java
+++ src/clinicadental/Principal.java
@@ -15,11 +15,8 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Holaa");
-    }
-    
-    public void Alaaa(){
         
     }
     
+   
 }
